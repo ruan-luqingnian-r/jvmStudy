1b6369af5889b13e0782c8dfa56afcde0d563710
ruan-luqingnian-r <15991875018@163.com>
2021-07-23 08:51:49 +0800

    Test01.java

--- jvm/src/test/Test01.java
+++ jvm/src/test/Test01.java
@@ -3,7 +3,16 @@ package test;
 /**
  * @Author: ruan
  * Date: 2021/7/23 8:49
- * @Description:
+ * @Description: java内存区域程序计数器
  */
 public class Test01 {
+    private Integer id;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
 }
