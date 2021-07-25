5bb200cf2b63908c31454c013b1dace45abe6164
ruan-luqingnian-r <15991875018@163.com>
2021-07-25 10:17:09 +0800

    TestMaxObject.java

--- jvm/src/test/TestMaxObject.java
+++ jvm/src/test/TestMaxObject.java
@@ -9,6 +9,10 @@ package test;
 public class TestMaxObject {
 
     public static void main(String[] args) {
+        /**
+         *设置新时代最大容量为3m时
+         * 创建10m的大对象这会直接被分配在老年代中
+         */
         byte[] bytes = new byte[1024 * 1024 * 10];
     }
 }
